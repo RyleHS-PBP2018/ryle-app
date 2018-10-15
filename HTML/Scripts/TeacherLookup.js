d6bc866acd4c22adabd38f97c01ab7ec8cca356e
NovaZure <jaytonward@yahoo.com>
2018-10-15 09:37:18 -0400

    Added Lookup Scripts
    Created a .json file of the Extension Grid, to be updated regularly. Also worked out some spacing and formatting in HTML

---
+++ HTML/Scripts/TeacherLookup.js
@@ -0,0 +1,13 @@
+//
+
+function GetInfoByRN() {
+    var mydata = JSON.parse("Extension Grid");
+    document.getElementById("ToRoomRNumber").innerHTML = "Paragraph changed.";
+	document.getElementById("ToRoomTeacher").innerHTML = "Paragraph changed.";
+	document.getElementById("ToRoomWebsite").innerHTML = "Paragraph changed.";
+	document.getElementById("ToRoomEmail").innerHTML = "Paragraph changed.";
+}
+
+function GetInfoByTeacher() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
