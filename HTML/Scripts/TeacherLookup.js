0b02d84c4e5c2e1626d3e7c287d011c546917bbd
NovaZure <jaytonward@yahoo.com>
2018-10-15 22:50:35 -0400

    Added JSON Reader

--- HTML/Scripts/TeacherLookup.js
+++ HTML/Scripts/TeacherLookup.js
@@ -1,11 +1,25 @@
-//
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
 
-function GetInfoByRN() {
-    var mydata = JSON.parse("Extension Grid");
-    document.getElementById("ToRoomRNumber").innerHTML = "Paragraph changed.";
+
+
+function GetInfoByRN(){
+
+	var data = loadFile("file://ExtensionGrid.json");
+	
+	document.getElementById("ToRoomRNumber").innerHTML = data[0]["Last Names"];
 	document.getElementById("ToRoomTeacher").innerHTML = "Paragraph changed.";
 	document.getElementById("ToRoomWebsite").innerHTML = "Paragraph changed.";
 	document.getElementById("ToRoomEmail").innerHTML = "Paragraph changed.";
+	
 }
 
 function GetInfoByTeacher() {
