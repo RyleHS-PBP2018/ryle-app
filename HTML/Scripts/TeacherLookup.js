341f2e42eee3edb77adae26e559fa319e07e3c18
NovaZure <jaytonward@yahoo.com>
2018-10-17 09:28:48 -0400

    Modified Teacher Lookup to use JQuery

--- HTML/Scripts/TeacherLookup.js
+++ HTML/Scripts/TeacherLookup.js
@@ -1,8 +1,15 @@
 
+var arrItems = []; //The array that will store the json values
 
-function GetInfoByRN(data){	
+$.getJSON("ExtensionGrid.json", function (data) { 
+	$.each(data, function (index, value) 
+		{ arrItems.push(value); })
+});
+
+
+function GetInfoByRN(){	
 	
-	document.getElementById("ToRoomRNumber").innerHTML = data[0]["Last Names"];
+	document.getElementById("ToRoomRNumber").innerHTML = arrItems[0]["Last Names"];
 	document.getElementById("ToRoomTeacher").innerHTML = "Paragraph changed.";
 	document.getElementById("ToRoomWebsite").innerHTML = "Paragraph changed.";
 	document.getElementById("ToRoomEmail").innerHTML = "Paragraph changed.";
