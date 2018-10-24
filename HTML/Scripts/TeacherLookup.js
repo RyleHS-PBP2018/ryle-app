e59e684abc8e370d8a926dcbeecbe4a26c08cb8a
NovaZure <jaytonward@yahoo.com>
2018-10-23 20:15:23 -0400

    Implemented Working Database Lookup Functions
    The .json database can now be accessed within TeacherLookup.js. Index.html has been commented for clarity and reorganized to match the order of the input forms.

--- HTML/Scripts/TeacherLookup.js
+++ HTML/Scripts/TeacherLookup.js
@@ -13,14 +13,47 @@ function readTextFile(file, callback) {
 }
 
 
-function GetInfoByRN(arr){
-	//document.getElementById("ToRoomRNumber").innerHTML = arr[0]["Last Names"];
-	document.getElementById("ToRoomTeacher").innerHTML = "Paragraph changed.";
-	document.getElementById("ToRoomWebsite").innerHTML = "Paragraph changed.";
-	document.getElementById("ToRoomEmail").innerHTML = "Paragraph changed.";
+function GetInfoByRN(FromTo){
+	let e = document.getElementById("ClassPeriod");
+	let period = e.options[e.selectedIndex].value;
+	
+	let roomnumber = parseInt(document.getElementById(FromTo + "RoomNumber").value);
+	
+	let iterator = 0;
+	let DBRoomNumber = "";
+	
+	//TODO Clean up logic errors (changes display to last index if room not found or teacher does not have [period])
+	while (iterator < database.data.length - 1) {
+		DBRoomNumber = database.data[iterator][period];
+		if (DBRoomNumber == null) { iterator++; continue }
+		DBRoomNumber = DBRoomNumber.replace(/\D/g,'');
+		
+		if (DBRoomNumber == roomnumber) {
+			console.log("GetInfoByRN: Room Data located!");
+			break;
+		}
+		
+		else { iterator++; }
+	}
+	
+	document.getElementById(FromTo + "RoomRNumber").innerHTML = "Room " + database.data[iterator][period];
+	document.getElementById(FromTo + "RoomTeacher").innerHTML = database.data[iterator]["Last Names"];
 	
 }
 
 function GetInfoByTeacher() {
    document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+// --------------- Seperation Between function definitions and actual scripts ----------------
+
+var database = {
+    data:undefined,
+	logData : function() { console.log(this.data); }
+}
+
+readTextFile("Scripts/ExtensionGrid.json", function(text){
+	var data = JSON.parse(text);
+	database["data"] = data;
+	console.log(database.data);
+}); 
