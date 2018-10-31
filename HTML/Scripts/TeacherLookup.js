61a8df757aa7c708dda6477f6f528449ab00edc5
NovaZure <jaytonward@yahoo.com>
2018-10-31 09:36:10 -0400

    Added Autocomplete for Teacher Lookup
    Added autocomplete, additional style markings to help with autocomplete. Also added a skeleton for the Teacher Lookup Utility and removed JQuery because it was not being used. Added teachernames array that holds all teacher names in a "Last Name, First Name" format.

--- HTML/Scripts/TeacherLookup.js
+++ HTML/Scripts/TeacherLookup.js
@@ -32,7 +32,7 @@ function GetInfoByRN(FromTo){ //TODO Handle room numbers that have letters in th
 
 
 function GetInfoByTeacher() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
+   
 }
 
 
@@ -86,7 +86,7 @@ function CheckErrorBoxes() {
 }
 
 
-function SwitchTab(ElementID) {
+function SwitchTab(ElementID) { //TODO Hide ErrorLog and reset its content when switching tabs
 	//If element specified is not already visible
 	if (document.getElementById(ElementID + "LookupTab").style.display != "block") {
 		
@@ -111,13 +111,121 @@ function SwitchTab(ElementID) {
 	} else { return false; }
 }
 
+
+function AddAutocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+} 
+
+
 // --------------- Seperation Between function definitions and actual scripts ----------------
 
 var database = {
     data:undefined,
+	teachernames: [],
+	
+	FillTeacherNames: function() {
+		for (i = 0; i < this.data.length; i++) {
+			this.teachernames.push(this.data[i]["Last Names"] + ", " + this.data[i]["First Names"]);
+		}
+	}
 }
 
 readTextFile("Scripts/ExtensionGrid.json", function(text){
 	var data = JSON.parse(text);
 	database["data"] = data;
+	database.FillTeacherNames();
+	AddAutocomplete(document.getElementById("TeacherNameInput"), database.teachernames);
 }); 
